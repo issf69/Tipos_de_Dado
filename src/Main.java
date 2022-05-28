9767a2cd26e40d988af65b673bee321d371e5948
Irene Silva França <irenesilvafranca2016@gmail.com>
2022-05-28 17:09:14 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+
+
+/**
+ * Classe de exemplo para exercício da aula 2 de variáveis, Tipo de dado e Operadores aritiméticos.
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 2000;
+        //short s2 = 4000;
+
+        //int 11 = -10000000000;
+        int i2 = 28500;
+
+        long l1 = 1000000000000000000L;
+        long l2 = 2004005000500055000L;
+
+        //float f1 = 4.5;
+        float f2 = 10.68F;
+
+        double d1 = 85.69;
+        double d2 = 99.04d;
+
+        char c1 = 'w';
+        //char c2 = 'tw';
+        char c3 = '\u0057';
+
+        String st1 = "Fulano";
+        String st2 = "Cicrano";
+        String st3 = "ag dgb se se et t KNBJBJBB &*'&%& 75894389";
+
+        //String dt1 = "09/02/1981";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(b1);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c3);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+    }
+}
+
